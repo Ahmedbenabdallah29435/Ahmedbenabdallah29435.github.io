38cfc3612db32eb3da642302ec21d8f877708164
Ahmed Ben Abdallah <63337471+Ahmedbenabdallah29435@users.noreply.github.com>
2025-02-27 23:10:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./Ahmedbenabdallah29435.github.io",
+  base: '/',  // Change this if needed based on your deployment URL
   build: {
-    minify: "terser",
+    minify: 'terser',
   },
 });
