69fc93d5e956ca2819e01a7152f0004768f06e11
Ahmed Ben Abdallah <63337471+Ahmedbenabdallah29435@users.noreply.github.com>
2025-02-27 23:45:33 +0100

    TEST

--- src/kaboomCtx.js
+++
@@ -1,9 +0,0 @@
-import kaboom from 'kaboom'; // Importing Kaboom from the installed npm package
-import { scaleFactor } from "./constants";
-
-export const k = kaboom({
-  global: false,
-  touchToMouse: true,
-  canvas: document.getElementById("game"),
-  debug: false, // set to false once ready for production
-});
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/',  // Change this if needed based on your deployment URL
-  build: {
-    minify: 'terser',
-  },
-});
